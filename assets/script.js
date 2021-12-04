952fbb47c262ea408ed8887f9c4bb67d95bd0f5c
mflanigan <mflanigantwualumn@gmail.com>
2021-12-04 14:21:56 -0600

    html setup and with basic css

---
+++ assets/script.js
@@ -0,0 +1,26 @@
+//buttons
+var btnStartEl = document.querySelector("#start-game");
+
+//question-answer elements
+var questionEl = document.getElementById("question")
+var answerbuttonsEl = document.getElementById("answer-buttons")
+var timerEl = document.querySelector("#timer");
+
+
+
+
+// The array of questions for quiz
+var questions = []
+
+// timer countdown
+
+
+//display correct/wrong
+
+
+// display total score on screen at end of game
+
+
+//high score button
+
+
