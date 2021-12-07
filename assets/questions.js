4a2bd287141c62307f50d9df27fe64d092d24ef8
mflanigan <mflanigantwualumn@gmail.com>
2021-12-06 18:10:16 -0600

    added questionTitle, timer, and startQuiz functions

---
+++ assets/questions.js
@@ -0,0 +1,13 @@
+// The array of questions for quiz
+var questions = [
+    {
+        title: "Inside which HTML element do we put the JavaScript?",
+        choices: ["a", "b", "c", "d"],
+        correct: "c",
+    },
+    {
+        title: "Where is the correct place to insert a JavaScript?",
+        choices: ["a", "b", "c", "d"],
+        correct: "a",
+    }
+]
--- assets/script.js
+++ assets/script.js
@@ -2,18 +2,60 @@
 var btnStartEl = document.querySelector("#start-game");
 
 //question-answer elements
-var questionEl = document.getElementById("question")
-var answerbuttonsEl = document.getElementById("answer-buttons")
+var questionEl = document.getElementById("question-container");
+var answerbuttonsEl = document.getElementById("answer-buttons");
 var timerEl = document.querySelector("#timer");
+var questionTitleEl = document.querySelector("#question");
+var starterContainerEl = document.getElementById("starter-container");
+var time = questions.length * 15;
+var timerId;
+var currentQuestionIndex = 0;
 
 
+function startQuiz() {
+    starterContainerEl.setAttribute("class", "hide");
+    questionEl.removeAttribute("class");
 
-
-// The array of questions for quiz
-var questions = []
+    timerId = setInterval(clock, 1000);
+    timerEl.textContent = time;
+    nextQuestion();
+};
 
 // timer countdown
+function clock() {
+    time--;
+    timerEl.textContent = time;
 
+    if (time < 0) {
+        endQuiz();
+    }
+};
+
+// end quiz
+function endQuiz() {
+
+};
+
+// next question
+function nextQuestion() {
+    var currentQuestion = questions[currentQuestionIndex];
+    questionTitleEl.textContent = currentQuestion.title;
+    answerbuttonsEl.innerHTML = "";
+
+    currentQuestion.choices.forEach(function (choice, i){
+        var answerChoice = document.createElement("button");
+        answerChoice.setAttribute("class", "choice");
+        answerChoice.setAttribute("value", choice);
+        answerChoice.textContent = i + 1 + ". " + choice;
+        answerChoice.onclick = questionClick;
+        answerbuttonsEl.appendChild(answerChoice);
+    })
+    
+};
+
+function questionClick() {
+    
+};
 
 //display correct/wrong
 
@@ -23,4 +65,4 @@ var questions = []
 
 //high score button
 
-
+btnStartEl.addEventListener("click", startQuiz);
