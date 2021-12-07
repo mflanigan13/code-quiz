6e82724754f15da1a622bd8834512948fe1792e4
mflanigan <mflanigantwualumn@gmail.com>
2021-12-06 18:23:27 -0600

    updated quiz questions

--- assets/questions.js
+++ assets/questions.js
@@ -2,12 +2,32 @@
 var questions = [
     {
         title: "Inside which HTML element do we put the JavaScript?",
-        choices: ["a", "b", "c", "d"],
-        correct: "c",
+        choices: ["a. <script>", "b. <javascript>", "c. <scripting>", "d. <js>"],
+        correct: "a",
     },
     {
         title: "Where is the correct place to insert a JavaScript?",
-        choices: ["a", "b", "c", "d"],
+        choices: ["a. The <head> section", "b The <body> section", "c. Both the <head> section and the <body> section are correct", "d. You don't have to insert."],
+        correct: "c",
+    },
+    {
+        title: "The external JavaScript file must contain the <script> tag.",
+        choices: ["true", "false"],
+        correct: "false",
+    },
+    {
+        title: "How do you create a function in JavaScript?",
+        choices: ["a. function myFunction()", "b. function = myFunction()", "c. function:myFunction()", "d. myFunction = myFunction[]"],
         correct: "a",
-    }
+    },
+    {
+        title: "How do you call a function named 'myFunction' ?",
+        choices: ["a. call myFunction()", "b. myFunction()", "c. call function myFunction", "d. myfunction==="],
+        correct: "b",
+    },
+    {
+        title: "How can you add a comment in a JavaScript?",
+        choices: ["a. <!--This is a Comment-->", "b. 'This is a Comment'", "c. //This is a Comment", "d> /*This is a comment"],
+        correct: "c",
+    },
 ]
