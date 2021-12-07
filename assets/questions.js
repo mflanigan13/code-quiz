fa2ccb7c629445f5394860fe7971ad4b8d2aea46
mflanigan <mflanigantwualumn@gmail.com>
2021-12-06 18:30:57 -0600

    updated questions with choices

--- assets/questions.js
+++ assets/questions.js
@@ -2,13 +2,13 @@
 var questions = [
     {
         title: "Inside which HTML element do we put the JavaScript?",
-        choices: ["a. <script>", "b. <javascript>", "c. <scripting>", "d. <js>"],
-        correct: "a",
+        choices: ["<script>", "<javascript>", "<scripting>", "<js>"],
+        correct: "<script>",
     },
     {
         title: "Where is the correct place to insert a JavaScript?",
-        choices: ["a. The <head> section", "b The <body> section", "c. Both the <head> section and the <body> section are correct", "d. You don't have to insert."],
-        correct: "c",
+        choices: ["The <head> section", "The <body> section", "Both the <head> section and the <body> section are correct", "You don't have to insert."],
+        correct: "Both the <head> section and the <body> section are correct",
     },
     {
         title: "The external JavaScript file must contain the <script> tag.",
@@ -17,17 +17,17 @@ var questions = [
     },
     {
         title: "How do you create a function in JavaScript?",
-        choices: ["a. function myFunction()", "b. function = myFunction()", "c. function:myFunction()", "d. myFunction = myFunction[]"],
-        correct: "a",
+        choices: ["function myFunction()", "function = myFunction()", "function:myFunction()", "myFunction = myFunction[]"],
+        correct: "function myFunction()",
     },
     {
         title: "How do you call a function named 'myFunction' ?",
-        choices: ["a. call myFunction()", "b. myFunction()", "c. call function myFunction", "d. myfunction==="],
-        correct: "b",
+        choices: ["call myFunction()", "myFunction()", "call function myFunction", "myfunction==="],
+        correct: "myFunction()",
     },
     {
         title: "How can you add a comment in a JavaScript?",
-        choices: ["a. <!--This is a Comment-->", "b. 'This is a Comment'", "c. //This is a Comment", "d> /*This is a comment"],
+        choices: ["<!--This is a Comment-->", "'This is a Comment'", "//This is a Comment", "*This is a comment"],
         correct: "c",
     },
 ]
