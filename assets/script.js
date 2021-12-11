cf10c0eac915933157d6bb00a678b9c32d6af105
mflanigan <mflanigantwualumn@gmail.com>
2021-12-11 15:37:02 -0600

    worked on high score elements

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,14 @@
 //buttons
-var btnStartEl = document.querySelector("#start-game");
+// var btnStartEl = document.querySelector("#start-game");
+var submitScoreBtn = document.getElementById("submit-score");
+var btnStartEl = document.getElementById("start-game");
 
 //question-answer elements
 var questionEl = document.getElementById("question-container");
 var endContainerEl = document.getElementById("end-container");
+var highScoreContainerEl = document.getElementById("high-score-container");
+var showScoreEl = document.getElementById("show-score");
+var scoreArray = [];
 var answerbuttonsEl = document.getElementById("answer-buttons");
 var timerEl = document.querySelector("#timer");
 var questionTitleEl = document.querySelector("#question");
@@ -36,11 +41,12 @@ function clock() {
 
 // end quiz
 function endQuiz() {
-    console.log("it works");
     questionEl.setAttribute("class", "hide");
     answerCorrectEl.setAttribute("class", "hide");
     answerWrongEl.setAttribute("class", "hide");
     endContainerEl.removeAttribute("class");
+    showScoreEl.innerHTML = "Score:" + totalScore;
+    submitScoreBtn.setAttribute("onclick", "formSubmit()");
 };
 
 // next question
@@ -79,14 +85,48 @@ function questionClick(c){
     currentQuestionIndex++;
     nextQuestion();
 
-
 };
 
+function formSubmit() {
+    var formInfoEl = document.getElementById('initials').value;
+    document.querySelector(".container").setAttribute("class", "hide");
 
+    var entry = {
+        "initials": formInfoEl,
+        "score": totalScore,
+    };
+    localStorage.setItem(localStorage.length, JSON.stringify(entry));
 
-// display total score on screen at end of game
+    // starterContainerEl.setAttribute("class", "hide");
+    // highScoreContainerEl.removeAttribute("class");
 
+    displayHighScore();
+};
 
-//high score button
+/*
+    displayHighScore() is a function which iterates over the items stored within the localStore
+    and adds each ofthem to a array. The array is then sorted from high to low based on each initials scores.
+*/
+function displayHighScore(){
+    for (let i = 0; i < localStorage.length; i++) {
+        const key = localStorage.key(i);
+        const entry = localStorage.getItem(key);
+        scoreArray.push(JSON.parse(entry));
+    }
+    
 
-btnStartEl.addEventListener("click", startQuiz);
+    scoreArray?.sort((entry1, entry2) => (entry1.score < entry2.score ? 1 : -1))
+    
+
+    // var mytable = "<table class=scoresTable`><tr>";
+    // for (var score of scoreArray) {  mytable += "<td>" + score + "</td>"; }
+    // mytable += "</tr></table>";
+    // document.getElementById("scoresTable").innerHTML = mytable;
+
+    for (let j =0; j < scoreArray.length; j++){
+        console.log(scoreArray[j].initials + " " + scoreArray[j].score)
+    }
+};
+
+// btnStartEl.addEventListener("click", startQuiz);
+btnStartEl.setAttribute("onclick", "startQuiz()");
